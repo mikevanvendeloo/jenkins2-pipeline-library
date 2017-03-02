471db8d79a725d5a167af84b96d2e743854c25a6
Mike van Vendeloo <mike@jpoint.nl>
2017-03-02 09:16:59 +0100

    Changed location to the src tree and introduced a package

--- src/vanvendeloo.jenkins/pipelineTools.groovy
+++ src/vanvendeloo.jenkins/pipelineTools.groovy
@@ -1,9 +1,5 @@
-#!/usr/bin/env groovy
-def isMaster = env.BRANCH_NAME == 'master'
-def isDevelop = env.BRANCH_NAME == 'develop'
-properties([
-        buildDiscarder(logRotator(numToKeepStr: '30'))
-])
+package vanvendeloo.jenkins
+
 def getCurrentVersion() {
     def matcher = readFile('pom.xml') =~ '<version>(.+?-.*)</version>'
     //matcher ? matcher[0][1].tokenize(".") : null
